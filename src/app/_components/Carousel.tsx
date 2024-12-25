2aa7123bfd96452a026f29a083dd5d2c850f7b44
24LP6831 24LP6831 <24LP6831@G302-CC0093.local>
2024-12-25 18:49:18 +0800

    first commit

---
+++ src/app/_components/Carousel.tsx
@@ -0,0 +1,9 @@
+import { Carousel } from "@/components/ui/carousel";
+
+export default function Carou() {
+    return (
+        <div className="flex justify-center items-center h-[510px]">
+        <Carousel />
+        </div>
+    );
+    }
